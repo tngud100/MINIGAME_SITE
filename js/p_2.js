eeb7839292b2856c11b8edb2619048dd648dede2
김수형 <>
2022-06-10 15:19:07 +0900

    First_game구현

--- js/p_2.js
+++ js/p_2.js
@@ -1,17 +1,13 @@
-<<<<<<< HEAD
 var time = 3;
 var enemy_state = 0; // 1:묵 2:찌 3:빠
-=======
 var time = 3; // 시간초
 var result = 0; // 0: 무승부, 1: Me_win, 2: AI_win
 var state_result = 0; // 0: 플레이 1: Me_win, 2: AI_win
 
-var enemy_state = []; // AI
-var my_state = []; // 유저
+var my_state = 0; // 유저
 var btn_count = 0; // 몇번째 게임인가
 
-var btn_check = 0; // 버튼 선택 여부
->>>>>>> ad9ea8a13cb59e34ce006a6481f0ffd230361e7a
+var btn_check = false; // 버튼 선택 여부
 
 $(function(){
     $(".game_page").hide();
@@ -20,7 +16,6 @@ $(function(){
         $(".game_page").show();
 
         timer();
-        play();
         
     });
 });
@@ -38,68 +33,73 @@ function timer(){
             $('#timer').text(time);
             setTimeout(()=>{
                 $('#time_circle').hide();
-                btn_check = 1;
+                $('#result').text("Choose!");
+                btn_check = true;
             },1000)
         }, 1000);
-<<<<<<< HEAD
-    });
-});
-
-function play(){
-    let x = Math.floor((Math.random() * 3) + 1);
-    enemy_state = x;
-    
-    var rock = document.getElementById("rock").name;
-}
-=======
     }, 1000);
-    
-}
+};
 
+
+function my_btn(player_num){
+    if(btn_check == true){
+        btn_check = false;
+        my_state = player_num;
+
+        game_rule(my_state);
+    }
+}
 
 function game_rule(my_state){ // 첫번째 게임
-    var randNum = Math. floor(Math. random() * 3); // 0:묵, 1:가위, 2:보
-    enemy_state.push(randNum);
+    var enemy = Math. floor(Math. random() * 3); // 0:묵, 1:가위, 2:보
 
-    var enemy = enemy_state[btn_count - 1];
-
-    if(my_state = 0){
+    if(my_state == 0){
         if(enemy == 0){
             result = 0;
+            $('#result').text("Draw!");
         }else if(enemy == 1){
             result = 1;
+            $('#result').text("You Attack!");
         }else if(enemy == 2){
             result = 2;
+            $('#result').text("AI Attack!");
         }
     }
 
-    if(my_state = 1){
+    if(my_state == 1){
         if(enemy == 0){
             result = 2;
+            $('#result').text("AI Attack!");
         }else if(enemy == 1){
             result = 0;
+            $('#result').text("Draw!");
         }else if(enemy == 2){
             result = 1;
+            $('#result').text("You Attack!");
         }
     }
 
-    if(my_state = 2){
+    if(my_state == 2){
         if(enemy == 0){
             result = 1;
+            $('#result').text("You Attack!");
         }else if(enemy == 1){
             result = 2;
+            $('#result').text("AI Attack!");
         }else if(enemy == 2){
             result = 0;
+            $('#result').text("Draw!");
         }
     }
-    return result;
 }
+/*
 
-function game_rule2(new_my_state){ // 유저 선공
+function game_rule2(){ // 유저 선공
     var randNum = Math. floor(Math. random() * 3); // 0:묵, 1:가위, 2:보
     enemy_state.push(randNum);
     
     var enemy = enemy_state[btn_count - 1];
+    my_state = btn_click(); // 버튼 선택
 
     if(my_state = 0){
         if(enemy == 0){
@@ -142,29 +142,33 @@ function game_rule2(new_my_state){ // 유저 선공
 //         묵	=>	묵 찌 빠		=>	짐(2), 이김 game_rule2 다시(1), 비김 game_rule3 다시(0)
 //         찌	=>	묵 찌 빠		=>	비김 game_rule3 다시(0), 짐(2), 이김 game_rule2 다시(1)
 //         빠	=>	묵 찌 빠		=>	이김 game_rule2 다시(1), 비김 game_rule3 다시(0), 짐(2)
-function game_rule3(new_my_state){ // AI 선공
+function game_rule3(){ // AI 선공
     var randNum = Math. floor(Math. random() * 3); // 0:묵, 1:가위, 2:보
     enemy_state.push(randNum);
-
 }
 
 function play(){        
+    var my_state = []; // 유저
     my_state = btn_click(); // 버튼 선택
 
+
     if( btn_check == 2){    // 버튼 선택 후
-        if(game_rule(my_state) == 0){
+        var my_btn = game_rule(my_state);
+
+        if(my_btn == 0){
             timer();
             play();
         }
-        if(game_rule(my_state) == 1){
+        if(my_btn == 1){
             timer();
-            game_rule2(my_state);
+            game_rule2();
         }
-        if(game_rule(my_state) == 2){
+        if(my_btn == 2){
             timer();
             game_rule3()
         }
     }
+}
 
 function btn_click(){
     if( btn_check == 1 ){ // 버튼 1번만 선택 가능
@@ -184,7 +188,6 @@ function btn_click(){
     btn_count += 1;
 
     return my_state[btn_count - 1];
-}
 }
 
 //     ■ 결과 1(game_rule1)
@@ -208,4 +211,4 @@ function btn_click(){
 //         묵	=>	묵 찌 빠		=>	짐(2), 이김 game_rule2 다시(1), 비김 game_rule3 다시(0)
 //         찌	=>	묵 찌 빠		=>	비김 game_rule3 다시(0), 짐(2), 이김 game_rule2 다시(1)
 //         빠	=>	묵 찌 빠		=>	이김 game_rule2 다시(1), 비김 game_rule3 다시(0), 짐(2)
->>>>>>> ad9ea8a13cb59e34ce006a6481f0ffd230361e7a
+*/
